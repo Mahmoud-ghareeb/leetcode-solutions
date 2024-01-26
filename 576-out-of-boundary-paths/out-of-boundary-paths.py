a737ff68be4baefdaf42e2136b4f98c269502a38
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-01-26 20:45:40 +0200

    Time: 81 ms (80.92%) | Memory: 22.5 MB (35.26%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -3,8 +3,7 @@ class Solution:
 
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         MOD = 10**9 + 7
-        memo = {}
-
+        @cache
         def rec(i, j, remaining_moves):
             if remaining_moves == 0: 
                 return 0
@@ -16,11 +15,7 @@ class Solution:
                 if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n:
                     paths += 1
                 else:
-                    if (new_i, new_j, remaining_moves - 1) in memo:
-                        paths += memo[(new_i, new_j, remaining_moves - 1)]
-                    else:
-                        memo[(new_i, new_j, remaining_moves - 1)] = rec(new_i, new_j, remaining_moves - 1)
-                        paths += memo[(new_i, new_j, remaining_moves - 1)]
+                    paths += rec(new_i, new_j, remaining_moves - 1)
 
             return paths
 
