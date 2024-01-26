e4b61e36d2f4fe369f0e7fa10ec6957eaa67ebb0
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-01-26 20:43:04 +0200

    Time: 78 ms (84.39%) | Memory: 22.6 MB (35.26%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        MOD = 10**9 + 7
+        @cache
+        def rec(i, j, remaining_moves):
+            if remaining_moves == 0: 
+                return 0
+
+            paths = 0
+            for di, dj in dirs:
+                new_i, new_j = i + di, j + dj
+
+                if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n:
+                    paths += 1
+                else:
+                    paths += rec(new_i, new_j, remaining_moves - 1)
+
+            return paths
+
+        return rec(startRow, startColumn, maxMove) % MOD
+
+        
