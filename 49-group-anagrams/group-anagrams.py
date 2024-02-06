cd1eb538f7b1c714f36d09376a9df6aa324a52ad
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-02-06 23:22:41 +0200

    Time: 84 ms (84.46%) | Memory: 20.2 MB (74.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        a = defaultdict(list)
         
-        sups = [''.join(sorted(s)) for s in strs]
-        sol = []
-        vis = set()
-        for i, s in enumerate(sups):
-            temp = []
-            for j in range(i, len(strs)):
-                if s == sups[j] and j not in vis:
-                    vis.add(strs[j])
-                    temp.append(strs[j])
-                    vis.add(j)
-            if temp != []:
-                sol.append(temp)
-
-
-
-        return sol
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            a[sorted_word].append(word)
+        
+        return a.values()
