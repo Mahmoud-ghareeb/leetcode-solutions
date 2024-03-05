cf1e13df30d864eae345d9deec660f35648353fa
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-03-05 22:35:14 +0200

    Time: 86 ms (23.50%) | Memory: 17.4 MB (48.50%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -8,19 +8,16 @@ class Solution:
             if s[p1] != s[p2]:
                 return p2 - p1 + 1
 
+            f_p1, f_p2 = 1, 1
             while True:
-                p1 += 1
-                if s[p1] != s[p1-1]:
-                    break
-                if p2 <= p1:
-                    return 0
-            
-            while True:
-                p2 -= 1
-                if s[p2] != s[p2+1]:
-                    break
-                if p2 <= p1:
-                    return 0
+                if f_p1: p1 += 1
+                if f_p2: p2 -= 1
+
+                if s[p1] != s[p1-1]: f_p1 = 0
+                if s[p2] != s[p2+1]: f_p2 = 0
+                if f_p1 == 0 and f_p2 == 0: break
+
+                if p2 <= p1: return 0
                     
         return 1 if p1 == p2 else 0
 
