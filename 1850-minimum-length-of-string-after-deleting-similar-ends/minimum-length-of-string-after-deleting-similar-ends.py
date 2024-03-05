e2ea42eabcd81228050b56fc657a20a286223ce7
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-03-05 22:15:39 +0200

    Time: 62 ms (85.00%) | Memory: 17.4 MB (73.50%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -5,7 +5,6 @@ class Solution:
         p1, p2 = 0, n-1
 
         while p1 < p2:
-            print(p1, p2)
             if s[p1] != s[p2]:
                 return p2 - p1 + 1
 
@@ -22,7 +21,7 @@ class Solution:
                     break
                 if p2 <= p1:
                     return 0
-        print(p1, p2)
+                    
         return 1 if p1 == p2 else 0
 
 
