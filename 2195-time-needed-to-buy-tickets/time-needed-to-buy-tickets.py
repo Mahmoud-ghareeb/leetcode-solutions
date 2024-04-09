b08e72279f58c0c933c1081ee2a5aa6cd16c1fa9
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-04-09 20:46:24 +0200

    Time: 37 ms (88.06%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,19 +1,13 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
 
-        q = deque()
-        for i in range(len(tickets)):
-            q.append(i)
-            
         sol = 0
-        while True:
-            if tickets[k] == 0: break
-            c = q.popleft()
-            if tickets[c]>0:
-               tickets[c] -= 1
-               sol += 1
-               q.append(c)
-        
+        for idx, num in enumerate(tickets):
+            if idx>k:
+                sol += min(tickets[k]-1, num)
+            else:
+                sol += min(tickets[k], num)
+
         return sol
 
         
