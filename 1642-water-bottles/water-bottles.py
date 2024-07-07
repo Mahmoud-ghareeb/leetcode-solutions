b81f668b88454f5d141f3ca63357469fda9e8ac7
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-07-08 01:33:34 +0300

    Time: 44 ms (5.70%) | Memory: 16.4 MB (97.50%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        sol = numBottles
+        while True:
+            sol += numBottles//numExchange
+            numBottles = (numBottles//numExchange) + (numBottles%numExchange)
+            if numBottles < numExchange:
+                break
+
+        return sol
+        # return numBottles + (numBottles//numExchange) + (((numBottles//numExchange) + (numBottles%numExchange))//numExchange)
+        
