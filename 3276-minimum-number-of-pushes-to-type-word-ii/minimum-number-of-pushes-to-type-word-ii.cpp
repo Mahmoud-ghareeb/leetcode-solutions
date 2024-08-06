2e57ba0f1c42053e4db06c4cbaa053a0add463ef
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-08-06 23:27:20 +0300

    Time: 179 ms (13.64%) | Memory: 28 MB (11.95%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        map<char, int> m;
+
+        for(auto &i: word)
+            m[i]++;
+
+        vector<pair<char, int>> v {m.begin(), m.end()};
+
+        sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+         });
+        
+        int sol = 0;
+        int mul = 0;
+        int i = 0;
+        for(auto &r : v)
+        {
+            if (i % 8 == 0)
+                mul += 1;
+                
+            sol += (mul * r.second);
+            ++i;
+        }
+        
+        return sol;
+    }
+};
