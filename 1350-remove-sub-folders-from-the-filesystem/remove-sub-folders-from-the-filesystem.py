6508a269374593ba18dfc1763bd06ca25abf7fb1
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-10-25 17:18:30 +0300

    Time: 139 ms (70.17%) | Memory: 41.4 MB (23.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,22 +1,47 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
 
-        folder = sorted(folder, key=lambda x: len(x))
+        class Trie:
+            def __init__(self):
+                self.children = {}
+                self.end = 0
+
+        def insert(root, v):
+            folders = v[1:].split('/')
+            curr = root
+
+            for f in folders:
+
+                if f not in curr.children:
+
+                    node = Trie()
+                    curr.children[f] = node
+
+                curr = curr.children[f]
+
+            curr.end = 1
+
+        root = Trie()
+        for i in folder:
+            insert(root, i)
 
         sol = []
 
-        for idx,i in enumerate(folder):
-            if i == 0:
-                continue
+        curr = root
+        
+        def dfs(node, path):
+            if node.end == 1:
+                sol.append(path)
+                return 
+            if not node.children:
+                return
+            
+            # print(node.children.keys())
+            # print(path)
+            for i, node in node.children.items():
+                dfs(node, path + '/' + i)
 
-            sol.append(i)
-            for j in range(idx+1, len(folder)):
-                if folder[j] == 0:
-                    continue
-                    
-                if folder[j][:len(i)+1] == i+'/':
-                    folder[j] = 0
-
+        dfs(curr, '')
 
         return sol
 
