bee8602afd792978166b049852144ba6aacfe974
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-11-23 22:28:26 +0200

    Time: 139 ms (14.58%) | Memory: 19.4 MB (59.23%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -17,17 +17,7 @@ class Solution:
             d[tmp] += 1
             d[flip(tmp)] += 1
 
-
         sol = max(d.values(), default=0)
-
-        # for i in d:
-        #     tmp = flip(i)
-        #     if tmp in d:
-        #         sol = max(sol, d[i] + d[tmp])
-            
-        #     if '0' not in i or '1' not in i:
-        #         sol = max(sol, d[i])
-
 
         return sol
 
