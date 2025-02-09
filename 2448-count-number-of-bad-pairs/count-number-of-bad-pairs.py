5841988c32364be52250c658087958247ea49c18
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2025-02-09 05:46:57 +0200

    Time: 407 ms (5.47%) | Memory: 39.7 MB (8.10%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        hel = [nums[i]-i for i in range(n)]
+
+        count_similar = Counter(hel)
+        sol = 0
+
+        def spc_fact(n):
+            # if n == 1: return 0
+            tmp = 0
+            for i in range(n-1, 0, -1):
+                tmp += i
+                print(i, tmp)
+            
+            return tmp
+
+        for i, j in count_similar.items():
+            sol += spc_fact(j)
+
+        return spc_fact(len(nums))-sol
+        
